f7d2ab5bac42ab55178b0eb6f99e7808c35bdf70
Lucas Barbosa <barbosagraciano07@gmail.com>
2026-03-12 13:33:17 -0300

    Finalizando formulário de Contato

---
+++ app/api/contact/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+import z from "zod";
+import axios from "axios";
+
+const bodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  message: z.string(),
+});
+
+const WEBHOOK_URL = process.env.WEBHOOK_URL!;
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+
+    const { name, email, message } = bodySchema.parse(body);
+
+    const messageData = {
+      embeds: [
+        {
+          title: "Message contact",
+          color: 0x4983f5,
+          fields: [
+            {
+              name: "Nome",
+              value: name,
+              inline: true,
+            },
+            {
+              name: "E-mail",
+              value: email,
+              inline: true,
+            },
+            {
+              name: "Mensagem",
+              value: message,
+            },
+          ],
+        },
+      ],
+    };
+
+    await axios.post(WEBHOOK_URL, messageData);
+
+    return NextResponse.json({
+      message: "Mensagem enviada com sucesso!",
+    });
+  } catch (err) {
+    console.error(err);
+    // Em vez de NextResponse.error(), use:
+    return NextResponse.json(
+      { message: "Erro ao processar a requisição" }, 
+      { status: 500 }
+    );
+  }
+}
--- app/components/contact-form/index.tsx
+++ app/components/contact-form/index.tsx
@@ -1,66 +1,87 @@
-'use client'
+"use client";
 
-import { HiArrowNarrowRight } from "react-icons/hi"
-import { Button } from "../button"
-import { SectionTitle } from "../section-title"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { type } from "os"
+import { useForm } from "react-hook-form";
+import { Button } from "../button";
+import { SectionTitle } from "../section-title";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { motion } from "framer-motion";
+import { fadeUpAnimation } from "@/app/lib/animations";
 
-const contactFormsSchema = z.object({
-    name: z.string().min(3).max(100),
-    email: z.string().email(),
-    message: z.string().min(1).max(500),
-})
+const contactFormSchema = z.object({
+  name: z.string().min(2).max(100),
+  email: z.string().email(),
+  message: z.string().min(1).max(500),
+});
 
-type ContactFormData = z.infer<typeof contactFormsSchema>
+type contactFormData = z.infer<typeof contactFormSchema>;
 
 export const ContactForm = () => {
-    const { handleSubmit, register } = useForm<ContactFormData>({
-        resolver: zodResolver(contactFormsSchema)
-    })
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<contactFormData>({
+    resolver: zodResolver(contactFormSchema),
+  });
 
-    const onSubmit = (data: ContactFormData) => {
-        console.log(data)
+  const onSubmit = async (data: contactFormData) => {
+    try {
+      await axios.post("/api/contact", data);
+      toast.success("Mensagem enviada com sucesso");
+      reset();
+    } catch {
+      toast.error("Falha ao enviar mensagem. Tente novamente.");
     }
+  };
 
-    return (
-        <section id="contact" className="py-16 px-6 md:py-32 flex items-center justify-center bg-gray-950">
-            <div className="w-full max-w-[420px] mx-auto">
-                <SectionTitle 
-                subtitle="contato"
-                title="Vamos Trabalhar Juntos? Entre em contato"
-                className="items-center text-center"
-                />
+  return (
+    <section
+      id="contact"
+      className="py-16 px-6 md:py-32 flex items-center justify-center bg-gray-950"
+    >
+      <div className="w-full max-w-[420px] mx-auto ">
+        <SectionTitle
+          subtitle="contato"
+          title="Vamos trabalhar juntos? Entre em contato"
+          className="text-center items-center"
+        />
 
-                <form className="mt-12 w-full flex flex-col gap-4"
-                onSubmit={handleSubmit(onSubmit)}
-                >
-                    <input 
-                    placeholder="Nome"
-                    className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 focus:ring-emerald-600"
-                    {...register("name")}
-                    />
-                    <input 
-                    placeholder="Email"
-                    type="email"
-                    className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 focus:ring-emerald-600"
-                    {...register("email")}
-                    />
-                    <textarea 
-                    placeholder="Mensagem"
-                    className="resize-none w-full h-[138px] bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 focus:ring-emerald-600"
-                    maxLength={500}
-                    {...register("message")}
-                    />
+        <motion.form
+          className="mt-12 w-full flex flex-col gap-4"
+          onSubmit={handleSubmit(onSubmit)}
+          {...fadeUpAnimation}
+          transition={{ duration: 0.5 }}
+        >
+          <input
+            placeholder="Nome"
+            className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            {...register("name")}
+          />
+          <input
+            placeholder="E-mail"
+            type="email"
+            className="w-full h-14 bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            {...register("email")}
+          />
+          <textarea
+            placeholder="Mensagem"
+            className="resize-none h-[138px] w-full bg-gray-800 rounded-lg placeholder:text-gray-400 text-gray-50 p-4 focus:outline-none focus:ring-2 focus:ring-blue-600"
+            {...register("message")}
+            maxLength={500}
+          />
 
-                    <Button className="h-max mx-auto mt-6 shadow-button">
-                        Enviar Mensagem
-                        <HiArrowNarrowRight size={18} />
-                    </Button>
-                </form>
-            </div>
-        </section>
-    )
-}
+          <Button
+            className="h-max mx-auto mt-6 shadow-button"
+            disabled={isSubmitting}
+          >
+            Enviar mensagem
+          </Button>
+        </motion.form>
+      </div>
+    </section>
+  );
+};
